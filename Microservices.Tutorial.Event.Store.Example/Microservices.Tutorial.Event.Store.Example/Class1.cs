d6e89e671e727e005ba705c9a5810041028899e0
Ceydakesgin5 <ceydakesgin778@gmail.com>
2025-02-17 23:08:13 +0300

    event sourcing

---
+++ Microservices.Tutorial.Event.Store.Example/Microservices.Tutorial.Event.Store.Example/Class1.cs
@@ -0,0 +1,64 @@
+﻿
+#region Giriş
+//using EventStore.Client;
+//using System.Text.Json;
+
+//string connectionString="esdb://admin/changeit@localhost:2113?tls=false&tlsVerifyCert=false";
+//var settings = EventStoreClientSettings.Create(connectionString);
+//var client = new EventStoreClient(settings);
+
+////event oluşuruldu
+//OrderPlacedEvent orderPlacedEvent = new()
+//{
+//    OrderId = 1,
+//    TotalAmount = 1000
+//};
+
+//EventData eventData = new(
+//    eventId: Uuid.NewUuid(),
+//    type: orderPlacedEvent.GetType().Name,
+//    data: JsonSerializer.SerializeToUtf8Bytes(orderPlacedEvent)
+//    );
+
+////streame gönderildi.
+//await client.AppendToStreamAsync(
+//    streamName: "order-stream",
+//    expectedState: StreamState.Any,
+//    eventData: new[] {eventData}
+//    );
+
+
+//var events= client.ReadStreamAsync(
+//    streamName: "order-stream",
+//    direction: Direction.Forwards,//olayları baştan sona doğru okur.
+//    revision: StreamPosition.Start //Streamdaki olayları kaçıncı eventten başlayarak okuycağını ifade eder.
+//    );
+
+
+//var datas= await events.ToListAsync();
+
+//await client.SubscribeToStreamAsync(
+//    streamName: "order-stream",
+//    start: FromStream.Start,
+//    //resolvedEvent: subscribe olacak eventi getirir.
+//    eventAppeared: async (streamSubscription, resolvedEvent, cancellationToken) =>
+//    {
+//        OrderPlacedEvent @event = JsonSerializer.Deserialize<OrderPlacedEvent>(resolvedEvent.Event.Data.ToArray());
+//        await Console.Out.WriteLineAsync(JsonSerializer.Serialize(@event));
+//    },
+//    subscriptionDropped: (streamSubscription, subscripitonDroppedReason, exception) => Console.WriteLine("Disconnection")
+//    //bağlantı kopukluğu olduğunda tetklenecek fonksiyon
+
+//    );
+
+//Console.Read();
+//class OrderPlacedEvent
+//{
+//    public int OrderId { get; set; }
+//    public int TotalAmount { get; set; }
+//}
+#endregion
+
+#region Bakiye
+
+#endregion
