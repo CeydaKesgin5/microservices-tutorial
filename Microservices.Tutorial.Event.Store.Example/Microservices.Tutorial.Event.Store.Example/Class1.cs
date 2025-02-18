998029baef3a59d76a323ce91aacf6991a6b032a
Ceydakesgin5 <ceydakesgin778@gmail.com>
2025-02-18 10:17:16 +0300

    subscription op.

--- Microservices.Tutorial.Event.Store.Example/Microservices.Tutorial.Event.Store.Example/Class1.cs
+++ Microservices.Tutorial.Event.Store.Example/Microservices.Tutorial.Event.Store.Example/Class1.cs
@@ -1,7 +1,7 @@
-﻿
+﻿using EventStore.Client;
+using System.Text.Json;
 #region Giriş
-//using EventStore.Client;
-//using System.Text.Json;
+
 
 //string connectionString="esdb://admin/changeit@localhost:2113?tls=false&tlsVerifyCert=false";
 //var settings = EventStoreClientSettings.Create(connectionString);
@@ -60,5 +60,182 @@
 #endregion
 
 #region Bakiye
+
+EventStoreService eventStoreService = new();
+
+AccountCreatedEvent accountCreatedEvent = new()
+{
+    AccountId = "12345",
+    CustomerId = "98765",
+    StartBalance = 0,
+    Date = DateTime.UtcNow.Date
+};
+MoneyDepositedEvent moneyDepositedEvent1 = new()
+{
+    AccountId="12345",
+    Amount=1000,
+    Date = DateTime.UtcNow.Date
+};
+MoneyDepositedEvent moneyDepositedEvent2= new()
+{
+    AccountId = "12345",
+    Amount = 500,
+    Date = DateTime.UtcNow.Date
+};
+MoneyTransferedEvent moneyTransferedEvent = new()
+{
+    AccountId = "12345",
+    Amount = 200,
+    Date = DateTime.UtcNow.Date
+};
+MoneyDepositedEvent moneyDepositedEvent3 = new()
+{
+    AccountId = "12345",
+    Amount = 50,
+    Date = DateTime.UtcNow.Date
+};
+MoneyTransferedEvent moneyTransferedEvent1 = new()
+{
+    AccountId = "12345",
+    Amount = 250,
+    Date = DateTime.UtcNow.Date
+};
+MoneyTransferedEvent moneyTransferedEvent2 = new()
+{
+    AccountId = "12345",
+    Amount = 150,
+    Date = DateTime.UtcNow.Date
+};
+MoneyDepositedEvent moneyDepositedEvent4 = new()
+{
+    AccountId = "12345",
+    Amount = 2000,
+    Date = DateTime.UtcNow.Date
+};
+
+//eventler EventStore'a gönderildi.
+await eventStoreService.AsyncToStreamAsync(
+    streamName: $"customer-{accountCreatedEvent.CustomerId}-stream",
+    new[]
+    {
+        eventStoreService.GenerateEventData(accountCreatedEvent),
+        eventStoreService.GenerateEventData(moneyDepositedEvent1),
+        eventStoreService.GenerateEventData(moneyDepositedEvent2),
+        eventStoreService.GenerateEventData(moneyTransferedEvent),
+        eventStoreService.GenerateEventData(moneyDepositedEvent3),
+        eventStoreService.GenerateEventData(moneyTransferedEvent1),
+
+        eventStoreService.GenerateEventData(moneyTransferedEvent2),
+        eventStoreService.GenerateEventData(moneyDepositedEvent4),
+    }
+    );
+
+BalanceInfo balanceInfo = new();
+//sonuç değeri eventler kümülatif olarak değerlendirilerek elde edildi.
+await eventStoreService.SubscribeToStreamAsync(
+    streamName: $"customer-{accountCreatedEvent.CustomerId}-stream",
+    async (streamSubscription, resolvedEvent, cancellationToken )=>
+    {
+        string eventType= resolvedEvent.Event.EventType;
+        object @event=JsonSerializer.Deserialize(resolvedEvent.Event.Data.ToArray(),Type.GetType(eventType));
+
+        switch (@event)
+        {
+            case AccountCreatedEvent e:
+                balanceInfo.AccountId = e.AccountId;
+                balanceInfo.Balance = e.StartBalance;
+                break;
+            case MoneyDepositedEvent e:
+                balanceInfo.Balance += e.Amount;
+                break;
+            case MoneyWithdrawnEvent e:
+                balanceInfo.Balance -= e.Amount;
+                break;
+            case MoneyTransferedEvent e:
+                balanceInfo.Balance -= e.Amount;
+                break;
+        }
+
+        await Console.Out.WriteLineAsync("*******Balance*******");
+        await Console.Out.WriteLineAsync(JsonSerializer.Serialize(balanceInfo));
+        await Console.Out.WriteLineAsync("*******Balance*******");
+        await Console.Out.WriteLineAsync("");
+        await Console.Out.WriteLineAsync("");
+
+    }
+    
+    );
+
+Console.Read();
+class EventStoreService
+{
+    EventStoreClientSettings GetEventStoreClientSettings(string connectionString = 
+        "esdb://admin/changeit@localhost:2113?tls=false&tlsVerifyCert=false")
+        => EventStoreClientSettings.Create(connectionString);
+
+    EventStoreClient Client { get => new EventStoreClient(GetEventStoreClientSettings()); }
+
+    public async Task AsyncToStreamAsync(string streamName, IEnumerable<EventData> eventData)
+    => await Client.AppendToStreamAsync(
+        streamName: streamName,
+        eventData: eventData,
+        expectedState: StreamState.Any
+        );
+    //GenerateEventData aracılığı ile eventi EventData formatına dönüştürdük.
+    public EventData GenerateEventData(object @event) =>
+        new(
+            eventId: Uuid.NewUuid(),
+            type: @event.GetType().Name,
+            data: JsonSerializer.SerializeToUtf8Bytes( @event )
+            );
+
+     public async Task SubscribeToStreamAsync(string streamName, Func<StreamSubscription, ResolvedEvent, CancellationToken
+        , Task> eventAppeared)
+        => Client.SubscribeToStreamAsync(
+            streamName: streamName,
+            start: FromStream.Start,
+            eventAppeared: eventAppeared,
+            subscriptionDropped: (x, y, z) => Console.WriteLine("Disconnected")
+            );
+
+
+}
+
+class BalanceInfo
+{
+    public string AccountId { get; set; }
+    public int Balance { get; set; }
+}
+class AccountCreatedEvent
+{
+    public string AccountId { get; set; }
+    public string CustomerId { get; set; }
+    public int StartBalance { get; set; }
+    public DateTime Date { get; set; }
+}
+
+class MoneyDepositedEvent
+{
+    public string AccountId { get; set; }
+    public int Amount { get; set; }
+    public DateTime Date { get; set; }
+}
+
+class MoneyWithdrawnEvent
+{
+    public string AccountId { get; set; }
+    public int Amount { get; set; }
+    public DateTime Date { get; set; }
+}
+
+class MoneyTransferedEvent
+{
+    public string AccountId { get; set; }
+    public string TargetAccountId { get; set; }
+    public int Amount { get; set; }
+    public DateTime Date { get; set; }
+}
+
+
 
 #endregion
--- Microservices.Tutorial.Event.Store.Example/Microservices.Tutorial.Event.Store.Example/obj/Release/net8.0/Microservices.Tutorial.Event.Store.Example.AssemblyInfo.cs
+++ Microservices.Tutorial.Event.Store.Example/Microservices.Tutorial.Event.Store.Example/obj/Release/net8.0/Microservices.Tutorial.Event.Store.Example.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Microservices.Tutorial.Event.Store.Example")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6e89e671e727e005ba705c9a5810041028899e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Microservices.Tutorial.Event.Store.Example")]
