a430ad80d6471b01251d10d7fd55016009963371
Ceydakesgin5 <ceydakesgin778@gmail.com>
2025-02-19 23:44:36 +0300

    mongoDb service op.

--- Microsevices.Tutorial.Event.Store.Read.Data.Example/Product.Event.Handler.Service/Program.cs
+++ Microsevices.Tutorial.Event.Store.Read.Data.Example/Product.Event.Handler.Service/Program.cs
@@ -1,7 +1,11 @@
 using Product.Event.Handler.Service;
+using Shared.Services;
+using Shared.Services.Abstractions;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+//builder.Services.AddHostedService<Worker>();
 
+builder.Services.AddSingleton<IEventStoreService, IEventStoreService>();
+builder.Services.AddSingleton<IMongoDBService,MongoDBService>();
 var host = builder.Build();
 host.Run();
